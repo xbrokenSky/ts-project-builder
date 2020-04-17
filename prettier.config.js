a30ba0ddb9c8bb9fef423252507ea7775b4a2667
brokenSky <doinikov@gmx.com>
2020-04-17 14:19:52 +0300

    prettier, eslint-plugin-prettier npm packages installed, prettier.config.js created, prettier settings added to eslintrc.json

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	useTabs: false,
+	printWidth: 120,
+	singleQuote: true,
+	trailingComma: 'all',
+	bracketSpacing: true,
+	jsxBracketSameLine: false,
+	tabWidth: 4,
+	semi: true,
+};
