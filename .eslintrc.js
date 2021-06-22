9c765c96dc80b40027e979414b202c8360701340
brokenSky <doinikov@gmx.com>
2021-06-22 12:15:57 +0300

    npm packages updated, array newline rules changed

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,21 +4,13 @@ module.exports = {
     parserOptions: {
         ecmaVersion: 2021,
         sourceType: 'module',
-        ecmaFeatures: {
-            jsx: true,
-        },
+        ecmaFeatures: { jsx: true },
     },
     plugins: ['react', '@typescript-eslint', 'import', 'react-hooks', 'prettier'],
     settings: {
-        react: {
-            version: 'detect',
-        },
-        'import/parsers': {
-            '@typescript-eslint/parser': ['.ts', '.tsx'],
-        },
-        'import/resolver': {
-            typescript: './tsconfig.json',
-        },
+        react: { version: 'detect' },
+        'import/parsers': { '@typescript-eslint/parser': ['.ts', '.tsx'] },
+        'import/resolver': { typescript: './tsconfig.json' },
     },
     env: {
         browser: true,
@@ -49,7 +41,14 @@ module.exports = {
         '@typescript-eslint/no-unused-vars': ['warn', { args: 'after-used' }],
         'no-multi-assign': [1],
         indent: ['off', 4, { SwitchCase: 1 }],
-        quotes: ['warn', 'single', { avoidEscape: true, allowTemplateLiterals: true }],
+        quotes: [
+            'warn',
+            'single',
+            {
+                avoidEscape: true,
+                allowTemplateLiterals: true,
+            },
+        ],
         'jsx-quotes': ['warn', 'prefer-double'],
         'comma-dangle': ['warn', 'always-multiline'],
         'max-len': [1, 120, 2, { ignoreComments: true }],
@@ -58,16 +57,54 @@ module.exports = {
         'arrow-body-style': ['warn', 'as-needed', { requireReturnForObjectLiteral: true }],
         'arrow-parens': [1, 'always'],
         'object-curly-newline': [
-            'off',
+            'warn',
             {
-                ObjectExpression: { multiline: true, minProperties: 1 },
-                ObjectPattern: { multiline: true, minProperties: 5 },
-                ImportDeclaration: { multiline: true, minProperties: 5 },
-                ExportDeclaration: { multiline: true, minProperties: 3 },
+                ObjectExpression: {
+                    multiline: true,
+                    minProperties: 2,
+                },
+                ObjectPattern: {
+                    multiline: true,
+                    minProperties: 4,
+                },
+                ImportDeclaration: {
+                    multiline: true,
+                    minProperties: 3,
+                },
+                ExportDeclaration: {
+                    multiline: true,
+                    minProperties: 5,
+                },
+            },
+        ],
+        'array-bracket-newline': [
+            1,
+            {
+                multiline: true,
+                minItems: 3,
+            },
+        ],
+        'array-element-newline': [
+            1,
+            {
+                ArrayExpression: {
+                    multiline: true,
+                    minItems: 3,
+                },
+                ArrayPattern: {
+                    multiline: true,
+                    minItems: 3,
+                },
             },
         ],
         'class-methods-use-this': [0],
-        'no-underscore-dangle': ['error', { allowAfterThis: true, allowAfterSuper: true }],
+        'no-underscore-dangle': [
+            'error',
+            {
+                allowAfterThis: true,
+                allowAfterSuper: true,
+            },
+        ],
         'import/extensions': [
             2,
             'ignorePackages',
@@ -81,12 +118,19 @@ module.exports = {
         'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
         'import/prefer-default-export': ['off'],
         'react/jsx-filename-extension': ['off'],
-        'react/jsx-indent': [2, 4],
+        'react/jsx-indent': [1, 4],
         'react/prefer-stateless-function': [0, { ignorePureComponents: true }],
         'react/prop-types': [1, { skipUndeclared: true }],
         'react/jsx-one-expression-per-line': [0, { allow: 'single-child' }],
         'react/destructuring-assignment': [0, 'always', { ignoreClassFields: false }],
-        'react/forbid-prop-types': [1, { forbid: ['any'], checkContextTypes: true, checkChildContextTypes: true }],
+        'react/forbid-prop-types': [
+            1,
+            {
+                forbid: ['any'],
+                checkContextTypes: true,
+                checkChildContextTypes: true,
+            },
+        ],
         'react/jsx-indent-props': [1, 4],
         'react/no-multi-comp': [0, { ignoreStateless: true }],
         'react-hooks/rules-of-hooks': 'error',
